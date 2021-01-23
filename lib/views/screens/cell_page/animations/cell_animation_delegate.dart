755666a0ede7285a2d2733c9e89f222dbf3b9a83
Brett Owers <owersbrett@gmail.com>
2021-01-23 13:55:20 -0700

    todo: infinite opacity animator

--- lib/views/screens/cell_page/animations/cell_animation_delegate.dart
+++ lib/views/screens/cell_page/animations/cell_animation_delegate.dart
@@ -1,5 +1,6 @@
 import 'package:cell_mobile/blocs/cell/cell_bloc.dart';
 import 'package:cell_mobile/models/organelle.dart';
+import 'package:cell_mobile/views/screens/cell_page/animations/genetic_animation.dart';
 import 'package:flutter/material.dart';
 
 import 'nucleolus.dart';
@@ -12,11 +13,47 @@ class CellAnimationDelegate {
       case Organelle.nucleolus:
         return Nucleolus(organelleInfo: organelleInfo);
         break;
+      case Organelle.nucleic_acids:
+        return Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: GeneticAnimation(
+            organelleInfo: organelleInfo,
+            fast: false,
+            right: false,
+          ),
+        );
+      case Organelle.rna:
+        return GeneticAnimation(
+          organelleInfo: organelleInfo,
+          fast: false,
+          right: true,
+        );
+      case Organelle.base_pairs:
+        return GeneticAnimation(
+          organelleInfo: organelleInfo,
+          fast: true,
+          right: false,
+        );
+      case Organelle.dna:
+        return GeneticAnimation(
+          organelleInfo: organelleInfo,
+          fast: true,
+          right: true,
+        );
+      case Organelle.nucleoplasm:
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: PlasmAnimation(child: image),
+        );
+      case Organelle.nuclear_membrane:
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: image,
+        );
       default:
     }
 
-   return image;
-   return animatedImage;
+    return image;
+    return animatedImage;
   }
 }
-
