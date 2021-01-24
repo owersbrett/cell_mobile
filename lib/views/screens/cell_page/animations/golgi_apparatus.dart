cffcd0af924b81654ddecf31a3684c0704b27c2f
Brett Owers <owersbrett@gmail.com>
2021-01-23 20:33:34 -0700

    initial golgi commit

---
+++ lib/views/screens/cell_page/animations/golgi_apparatus.dart
@@ -0,0 +1,29 @@
+import 'package:cell_mobile/models/organelle.dart';
+import 'package:cell_mobile/views/screens/cell_page/animations/ribosome_animation.dart';
+import 'package:flutter/material.dart';
+
+class GolgiApparatus extends StatefulWidget {
+  GolgiApparatus({this.organelleInfo});
+  final OrganelleInfo organelleInfo;
+  @override
+  _GolgiApparatusState createState() => _GolgiApparatusState();
+}
+
+class _GolgiApparatusState extends State<GolgiApparatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+          padding: const EdgeInsets.only(bottom: 11.0, right: 4),
+          child: Transform.translate(
+            offset: Offset(-15, 10),
+            child: Transform.scale(
+              scale: .75,
+              child: RibosomeAnimation(
+                path: widget.organelleInfo.mainImagePath,
+                persistant: false,
+              ),
+            ),
+          ),
+        );
+  }
+}
