d9375b16bb4b91d5215f794778e8010e67ea8f3a
Brett Owers <owersbrett@gmail.com>
2021-01-26 02:36:20 -0700

    updated version and fixed shared preferences error

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'my_app.dart';
 
 import 'config_reader.dart';
 
 
-Future main(String env) async {
+Future main() async {
   // Always call this if the main method is asynchronous
   WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance();
+
   // Load the JSON config into memory 
   await ConfigReader.initialize();
 
