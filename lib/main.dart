9e0cc6f33a887a14c333c6eddd4c9a07bf539179
Brett Owers <owersbrett@gmail.com>
2021-01-26 01:06:35 -0700

    updated images

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'my_app.dart';
 
 import 'config_reader.dart';
-import 'environment.dart';
+
 
 Future main(String env) async {
   // Always call this if the main method is asynchronous
